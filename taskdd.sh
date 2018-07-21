00b8b929b6a2d6c551448df7da619db662cdaa56
Sean Henderson <mail@sean.sh>
2018-07-21 16:02:30 -0700

    Print all `task`s.
    Also some ideas on implementation.

--- taskdd.sh
+++ taskdd.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
 numTasks=$(task +PENDING count)
-echo "$numTasks"
+echo "$numTasks to consider"
+
+allTasks=()
+
+for ((i=1;i<=numTasks;i++))
+do
+    # TODO: save this task's project, annotations, and tags to a single line in
+    # a temp file to reference later.
+
+    allTasks+=("$(task _get $i.description)")
+
+    # TODO: if it's already in the array, delete it
+done
+
+for task in "${allTasks[@]}"
+do
+    echo "$task"
+done
+
+numTasksDeleted=$(expr $numTasks - $(task +PENDING count))
+echo "Deleted $numTasksDeleted"
